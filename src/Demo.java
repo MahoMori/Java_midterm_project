29145bd970c0a186eba1fd380d0e8dc97bee9599
MahoMori <83236914+MahoMori@users.noreply.github.com>
2021-06-15 10:26:54 -0700

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -5,11 +5,11 @@ import animal_name.Eel;
 public class Demo {
     public static void main(String[] args) {
         
-        Crocodile crocodile = new Crocodile("dry skin", "backbone", "soft-shelled eggs", 123, 123, "reptile", "cold-blooded");
+        Crocodile crocodile = new Crocodile();
 
-        Eel eel = new Eel("release electric charge", "live in water", "has gills", 123, 123, "fish", "cold-blooded");
+        Eel eel = new Eel();
 
-        Crocodile_bird crocodile_bird = new Crocodile_bird("with feathers", "can fly", 123, 123, "bird", "warm-blooded");
+        Crocodile_bird crocodile_bird = new Crocodile_bird();
 
         System.out.println("Crocodile: " + crocodile.getSkin() + ", " + crocodile.getBone() + ", " + crocodile.getEggs() + ", " + crocodile.getHeight() + ", " + crocodile.getWeight() + ", " + crocodile.getAnimalType() + ", " + crocodile.getBloodType());
 
